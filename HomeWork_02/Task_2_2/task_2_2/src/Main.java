b6e1cc1bef0bf098c66b28bd2c3c73d0fa881984
AKChizhov <ChizhovAK2007@yandex.ru>
2023-07-01 22:28:56 +0300

    Добавлено решение задачи №2 к семинару №2

--- HomeWork_02/Task_2_2/task_2_2/src/Main.java
+++ HomeWork_02/Task_2_2/task_2_2/src/Main.java
@@ -3,7 +3,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class Main {
     public static void main(String[] args){
         getFloat();
@@ -19,7 +18,6 @@ public class Main {
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
-
     }
     public static float getFloatFromUser(BufferedReader bufferedReader){
         final String RD  = "\u001B[31m"; //ANSI_RED
